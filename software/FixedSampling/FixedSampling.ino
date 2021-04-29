b0a24fe35f71351d47e1bdfd9cf21f5d979bfc7c
Deepak Khatri <deepaklorkhatri7@gmail.com>
2021-04-29 23:56:25 +0530

    Fixed Sampling code
    This code allows setting fixed sampling rate for the EXG pill ADC input.

---
+++ software/FixedSampling/FixedSampling.ino
@@ -0,0 +1,50 @@
+// Fixed Sampling - EXG Pill
+
+// Copyright (c) 2021 Upside Down Labs
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#define SampleRate 125
+#define BaudRate 115200
+#define inputPin A0
+
+
+void setup() {
+	// Serial connection begin
+	Serial.begin(BaudRate);
+}
+
+void loop() {
+	// Calculate elapsed time
+	static unsigned long past = 0;
+	unsigned long present = micros();
+	unsigned long interval = present - past;
+	past = present;
+
+	// Run timer
+	static long timer = 0;
+	timer -= interval;
+
+	// Sample
+	if(timer < 0){
+		timer += 1000000 / SampleRate;
+		int sensorValue = analogRead(inputPin);
+		Serial.println(sensorValue);
+	}
+}
