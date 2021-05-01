63f664a96b83370daa98cac84284e3d9c4cec2c5
Deepak Khatri <deepaklorkhatri7@gmail.com>
2021-05-02 01:30:10 +0530

    add ECGFilter code
    This code is useful for filtering ECG signal sampled with BioAmp EXG pill. This is not required under normal circumstances as the raw output from EXG Pill is very clean as it is :)

---
+++ software/ECGFilter/ECGFilter.ino
@@ -0,0 +1,91 @@
+// ECG Filter - EXG Pill
+
+// Copyright (c) 2021 Upside Down Labs
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#define SampleRate 125
+#define BaudRate 115200
+#define inputPin A0
+
+
+void setup() {
+	// Serial connection begin
+	Serial.begin(BaudRate);
+}
+
+void loop() {
+	// Calculate elapsed time
+	static unsigned long past = 0;
+	unsigned long present = micros();
+	unsigned long interval = present - past;
+	past = present;
+
+	// Run timer
+	static long timer = 0;
+	timer -= interval;
+
+	// Sample
+	if(timer < 0){
+		timer += 1000000 / SampleRate;
+		float sensorValue = analogRead(inputPin);
+		float ECGSignal = ECGFilter(sensorValue);
+		Serial.println(ECGSignal);
+	}
+}
+
+// Band-Pass Butterworth IIR digital filter, generated using filter_gen.py.
+// Sampling rate: 125.0 Hz, frequency: [0.5, 44.5] Hz.
+// Filter is order 4, implemented as second-order sections (biquads).
+// Reference:
+// https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.butter.html
+// https://courses.ideate.cmu.edu/16-223/f2020/Arduino/FilterDemos/filter_gen.py
+float ECGFilter(float input)
+{
+  float output = input;
+  {
+    static float z1, z2; // filter section state
+    float x = output - 0.70682283*z1 - 0.15621030*z2;
+    output = 0.28064917*x + 0.56129834*z1 + 0.28064917*z2;
+    z2 = z1;
+    z1 = x;
+  }
+  {
+    static float z1, z2; // filter section state
+    float x = output - 0.95028224*z1 - 0.54073140*z2;
+    output = 1.00000000*x + 2.00000000*z1 + 1.00000000*z2;
+    z2 = z1;
+    z1 = x;
+  }
+  {
+    static float z1, z2; // filter section state
+    float x = output - -1.95360385*z1 - 0.95423412*z2;
+    output = 1.00000000*x + -2.00000000*z1 + 1.00000000*z2;
+    z2 = z1;
+    z1 = x;
+  }
+  {
+    static float z1, z2; // filter section state
+    float x = output - -1.98048558*z1 - 0.98111344*z2;
+    output = 1.00000000*x + -2.00000000*z1 + 1.00000000*z2;
+    z2 = z1;
+    z1 = x;
+  }
+  return output;
+}
