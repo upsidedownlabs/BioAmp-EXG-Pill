14cc6c98d6f1467f24993b55809eac7d5153f12f
Aryan-Prakhar <97614212+Aryan-Prakhar@users.noreply.github.com>
2024-07-19 17:53:24 +0530

    Update OLED_BPM.ino
    Added library author name and made some grammatical correction

--- software/OLED_BPM/OLED_BPM.ino
+++ software/OLED_BPM/OLED_BPM.ino
@@ -1,35 +1,39 @@
- /*OLED_BPM - BioAmp EXG Pill
- https://github.com/upsidedownlabs/BioAmp-EXG-Pill
- https://github.com/upsidedownlabs/Heart-BioAmp-Arduino-Firmware
+ // OLED_BPM - BioAmp EXG Pill
+ // https://github.com/upsidedownlabs/BioAmp-EXG-Pill
+ // https://github.com/upsidedownlabs/Heart-BioAmp-Arduino-Firmware
 
- Upside Down Labs invests time and resources providing this open source code,
- please support Upside Down Labs and open-source hardware by purchasing
- products from Upside Down Labs!
+ // Upside Down Labs invests time and resources providing this open source code,
+ // please support Upside Down Labs and open-source hardware by purchasing
+ // products from Upside Down Labs!
 
- Copyright (c) 2021 - 2024 Upside Down Labs - contact@upsidedownlabs.tech
- Copyright (c) 2021 - 2024 Aryan Prakhar - aryanprakhar1010@gmail.com
- Copyright (c) 2021 - 2024 Dev Saran Sujan - devsaransujan@gmail.com
+ // Copyright (c) 2021 - 2024 Upside Down Labs - contact [at] upsidedownlabs.tech
+ // Copyright (c) 2021 - 2024 Aryan Prakhar - aryanprakhar1010 [at] gmail.com
+ // Copyright (c) 2021 - 2024 Dev Saran Sujan - devsaransujan [at] gmail.com
 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
+ // Permission is hereby granted, free of charge, to any person obtaining a copy
+ // of this software and associated documentation files (the "Software"), to deal
+ // in the Software without restriction, including without limitation the rights
+ // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ // copies of the Software, and to permit persons to whom the Software is
+ // furnished to do so, subject to the following conditions:
 
- The above copyright notice and this permission notice shall be included in all
- copies or substantial portions of the Software.
+ // The above copyright notice and this permission notice shall be included in all
+ // copies or substantial portions of the Software.
 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- SOFTWARE.*/
-
+ // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ // AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ // SOFTWARE.
 
 // Include necessary libraries
+// By Adafruit
+//https://github.com/adafruit/Adafruit_SSD1306/blob/master/Adafruit_SSD1306.h
+//By Roberto Lo Giacco
+// https://github.com/rlogiacco/CircularBuffer/blob/master/CircularBuffer.hpp
+
 #include <Adafruit_SSD1306.h>
 #include <Wire.h>
 #include <math.h>
